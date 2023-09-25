c10908cd7d72bf78a10cbdd266c2bf76cf64e130
mb-tousif <meta.block.tousif@gmail.com>
2023-09-25 14:22:18 +0600

    dashboard side navbar responsived done

--- src/components/DashboardNav.tsx
+++ src/components/DashboardNav.tsx
@@ -8,6 +8,7 @@ import { RiAccountBoxFill } from "react-icons/ri";
 import { GiTakeMyMoney, GiProfit } from "react-icons/gi";
 import { AiOutlineStock } from "react-icons/ai";
 import { TbTransferOut } from "react-icons/tb";
+import { HiHome } from "react-icons/hi";
 
 export default function DashboardNav() {
   const [toggle, setToggle] = React.useState(false);
@@ -19,85 +20,81 @@ export default function DashboardNav() {
   }
   return (
     <div className="sm:min-w-[38vh] text-left top-0 pb-6 left-0 min-h-screen bg-gradient-to-b from-[#D0DAE3] to-[#ADCDED]">
-      <div className="m-2 mb-2 mt-20 flex">
-        <BiSolidDashboard className="w-6 h-8" />
-        <Link
-          href="/dashboard"
-          className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-        >
-          <span>Dashboard</span>
+      <div className="m-2 mb-6 mt-14">
+        <Link href="/">
+          <HiHome className="mx-auto my-auto w-8 h-8" />
         </Link>
       </div>
-      <div className="m-2 flex">
-        <MdAdminPanelSettings className="w-6 h-8" />
-        <Link
-          href="/dashboard/admin"
-          className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-        >
-          <span>Admin</span>
+      <div className="m-2 mb-2">
+        <Link href="/dashboard" className="flex">
+          <BiSolidDashboard className="w-6 h-8" />
+          <span className="sm:flex hidden text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+            Dashboard
+          </span>
         </Link>
       </div>
-      <div className="m-2 flex">
-        <RiAccountBoxFill className="w-6 h-8" />
-        <Link
-          href="/dashboard/user"
-          className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-        >
-          <span>My Profile</span>
+      <div className="m-2">
+        <Link href="/dashboard/admin" className="flex">
+          <MdAdminPanelSettings className="w-6 h-8" />
+          <span className="sm:flex hidden text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+            Admin
+          </span>
         </Link>
       </div>
-      <div className="m-2 flex">
-        <ImProfile className="w-6 h-8" />
-        <Link
-          href="/dashboard/wallet"
-          className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-        >
-          <span>My Account</span>
+      <div className="m-2">
+        <Link href="/dashboard/user" className="flex">
+          <RiAccountBoxFill className="w-6 h-8" />
+          <span className="sm:flex hidden text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+            My Profile
+          </span>
         </Link>
       </div>
-      <div className="m-2 flex">
-        <MdOutlineGetApp className="w-6 h-8" />
-        <Link
-          href="/dashboard/deposits"
-          className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-        >
-          <span>Deposits</span>
+      <div className="m-2">
+        <Link href="/dashboard/wallet" className="flex">
+          <ImProfile className="w-6 h-8" />
+          <span className="sm:flex hidden text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+            My Account
+          </span>
         </Link>
       </div>
-      <div className="m-2 flex">
-        <TbTransferOut className="w-6 h-8" />
-        <Link
-          href="/dashboard/transfers"
-          className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-        >
-          <span>Transfer</span>
+      <div className="m-2">
+        <Link href="/dashboard/deposits" className="flex">
+          <MdOutlineGetApp className="w-6 h-8" />
+          <span className="sm:flex hidden text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+            Deposits
+          </span>
         </Link>
       </div>
-      <div className="m-2 flex">
-        <GiTakeMyMoney className="w-6 h-8" />
-        <Link
-          href="/dashboard/withdraws"
-          className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-        >
-          <span>Withdraw</span>
+      <div className="m-2">
+        <Link href="/dashboard/transfers" className="flex">
+          <TbTransferOut className="w-6 h-8" />
+          <span className="sm:flex hidden text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+            Transfer
+          </span>
         </Link>
       </div>
-      <div className="m-2 flex">
-        <AiOutlineStock className="w-6 h-8" />
-        <Link
-          href="/dashboard/stakes"
-          className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-        >
-          <span>My Stake</span>
+      <div className="m-2">
+        <Link href="/dashboard/withdraws" className="flex">
+          <GiTakeMyMoney className="w-6 h-8" />
+          <span className="sm:flex hidden text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+            Withdraw
+          </span>
         </Link>
       </div>
-      <div className="m-2 flex">
-        <GiProfit className="w-6 h-8" />
-        <Link
-          href="/dashboard/revenues"
-          className="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-        >
-          <span>Revenues</span>
+      <div className="m-2">
+        <Link href="/dashboard/stakes" className="flex">
+          <AiOutlineStock className="w-6 h-8" />
+          <span className="sm:flex hidden text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+            My Stake
+          </span>
+        </Link>
+      </div>
+      <div className="m-2">
+        <Link href="/dashboard/revenues" className="flex">
+          <GiProfit className="w-6 h-8" />
+          <span className="sm:flex hidden text-sm font-medium text-gray-700 py-2 px-2 hover:bg-[#1c3a13] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out">
+            Revenues
+          </span>
         </Link>
       </div>
     </div>
