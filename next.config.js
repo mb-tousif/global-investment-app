ce9d270d38dbd0e8ced44c22be5689201b996306
mb-tousif <meta.block.tousif@gmail.com>
2023-09-28 23:03:00 +0600

    ---

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['localhost', 'images.unsplash.com'],
-    },
-}
+  images: {
+    domains: ["localhost", "images.unsplash.com", "raw.githubusercontent.com"],
+  },
+};
 
 module.exports = nextConfig
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import Table from "@/components/Table";
 import React from "react";
 
 export default function Dashboard() {
   return (
     <div className="overflow-scroll min-h-full">
       <h1 className="text-center">Dashboard Home</h1>
+      <Table />
     </div>
   );
 }
